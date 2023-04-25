7d79a57f2b7c57381b3df062b489fca0eba443bd
Aria14cs <y.condo16@gmail.com>
2023-04-25 10:27:37 -0500

    Funcion mdLinks

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const { fileMd, leerFile }=require('./funciones.js');
+const {resolveRuta,fileExists, fileMd, leerFile,extraerLink, valLinks} = require('./funciones.js');
 
-const mdLinks = (path,option) =>{
-  return new Promise((reject,resolve)=>{ //leer,resolve
-   
-  })
+const mdLinks = (ruta,option) =>{ //dos parametros
+  return new Promise((resolve,reject)=>{ //leer,resolve
+   if(resolveRuta(ruta)){ //recibe ruta del archivo convertido
+    console.log(resolveRuta(ruta,'ruta'))
+  
+    resolve(resolveRuta(ruta));
+  } else{
+    reject(new Error('la ruta debe ser relativa'));
+  }
+  });
+} ;
 
-}
+
+const ruta= 'README.md';
+const option={validate:true};
+
+console.log(mdLinks)
+mdLinks(ruta,option)
+.then((result)=>{
+  console.log(result)
+})
+.catch((error)=>{
+  console.log(error)
+})
+// leerFile(rutaR)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
